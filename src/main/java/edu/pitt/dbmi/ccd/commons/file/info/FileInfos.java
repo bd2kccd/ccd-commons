377e7f02e31634354e134cd252715ec22077dc5d
Kevin Bui <kvb2univpitt@gmail.com>
2015-07-22 14:46:42 -0400

    Added new method delimiterStringToChar.

--- src/main/java/edu/pitt/dbmi/ccd/commons/file/info/FileInfos.java
+++ src/main/java/edu/pitt/dbmi/ccd/commons/file/info/FileInfos.java
@@ -18,7 +18,11 @@
  */
 package edu.pitt.dbmi.ccd.commons.file.info;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -34,7 +38,97 @@ import java.util.List;
  */
 public class FileInfos {
 
+    private static final byte NEW_LINE = '\n';
+
+    private static final byte CARRIAGE_RETURN = '\r';
+
     private FileInfos() {
+    }
+
+    public static char delimiterStringToChar(String delimiter) {
+        switch (delimiter) {
+            case "tab":
+                return '\t';
+            case "comma":
+                return ',';
+            default:
+                throw new IllegalArgumentException(String.format("Unknow delimiter: %s", delimiter));
+        }
+    }
+
+    /**
+     * Counts the number of column of the first line in the file.
+     *
+     * @param file dataset
+     * @param delimiter a single character used to separate the data
+     * @return
+     * @throws IOException
+     */
+    public static int countColumn(File file, char delimiter) throws IOException {
+        int count = 0;
+
+        byte delim = (byte) delimiter;
+        try (FileChannel fc = new RandomAccessFile(file, "r").getChannel()) {
+            MappedByteBuffer buffer = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+            byte currentChar = -1;
+            byte prevChar = NEW_LINE;
+            while (buffer.hasRemaining()) {
+                currentChar = buffer.get();
+                if (currentChar == CARRIAGE_RETURN) {
+                    currentChar = NEW_LINE;
+                }
+
+                if (currentChar == delim || (currentChar == NEW_LINE && prevChar != NEW_LINE)) {
+                    count++;
+                    if (currentChar == NEW_LINE) {
+                        break;
+                    }
+                }
+
+                prevChar = currentChar;
+            }
+
+            // take care of cases where there's no newline at the end of the file
+            if (!(currentChar == -1 || currentChar == NEW_LINE)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    /**
+     * Counts the number of lines that contain data.
+     *
+     * @param file dataset
+     * @return
+     * @throws IOException
+     */
+    public static int countLine(File file) throws IOException {
+        int count = 0;
+
+        try (FileChannel fc = new RandomAccessFile(file, "r").getChannel()) {
+            MappedByteBuffer buffer = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+            byte prevChar = NEW_LINE;
+            while (buffer.hasRemaining()) {
+                byte currentChar = buffer.get();
+                if (currentChar == CARRIAGE_RETURN) {
+                    currentChar = NEW_LINE;
+                }
+
+                if (currentChar == NEW_LINE && prevChar != NEW_LINE) {
+                    count++;
+                }
+
+                prevChar = currentChar;
+            }
+
+            if (prevChar != NEW_LINE) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
     public static BasicFileInfo basicPathInfo(Path path) throws IOException {
